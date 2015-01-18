8811e23ec002f0168c970445092cffc0a1dccb25
Gabriel Terwesten <gabriel@terwesten.net>
2015-01-19 00:21:57 +0100

    add mongoDB to travis ci config

--- test/utils/db.js
+++ test/utils/db.js
@@ -2,14 +2,21 @@
 var mongoose = module.exports.mongoose = require('mongoose');
 
 module.exports.connect = function (done) {
-    // Connect to mongoDB using env variables set because container
-    // running mongod is linked to container running the tests
-    mongoose.connect("mongodb://" +
-        process.env.MONGO_PORT_27017_TCP_ADDR + ":" +
-        process.env.MONGO_PORT_27017_TCP_PORT + "/test",
-        function (err) {
-            done(err);
-        });
+    if (process.env.RUN_ENV == 'travis') {
+        mongoose.connect("mongodb://127.0.0.1/test",
+            function (err) {
+                done(err);
+            });
+    } else {
+        // Connect to mongoDB using env variables set because container
+        // running mongod is linked to container running the tests
+        mongoose.connect("mongodb://" +
+            process.env.MONGO_PORT_27017_TCP_ADDR + ":" +
+            process.env.MONGO_PORT_27017_TCP_PORT + "/test",
+            function (err) {
+                done(err);
+            });
+    }
 }
 
 module.exports.disconnect = function (done) {
